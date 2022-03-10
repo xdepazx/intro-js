a974e0ed8d007dad7bd9cd1ea4f4a0fd10990d6c
Frida de Paz <frida.l.k.t2@gmail.com>
2022-03-09 22:33:48 -0600

    actividad 3

---
+++ actividad-3.js
@@ -0,0 +1,51 @@
+//ejercicio 1
+// var numero = Number(prompt("escribe un numero"))
+
+var numero = 29;
+
+if (numero % 2 === 0){
+    console.log('numero es divisible entre 2');
+}else{
+    console.log('No es divisible entre 2');
+}
+
+//ejercicio 2
+
+var numero = Number(prompt('introduce un numero'))
+var numero = 88;
+if(numero%2 == 0){
+    alert("el número" + numero + 'Es par');
+}else{
+    alert("el numero" + numero + 'no es par');
+}
+
+//ejercicio 3 
+
+var numero = 99;
+if(numero === 1000){
+    console.log('Ganaste un premio')
+}else{
+    console.log('sigue participando');
+}
+
+// alter
+
+
+//ejercicio 4
+var numero1 = 33;
+var numero2 = 89;
+if(numero1>numero2){
+    console.log('es menor que el primer numero');
+}if else(numero1<numero2){
+    console.log('es menor que el segundo numero');
+}if else(numero1==numero2){
+    null
+} 
+
+// alt correcta 
+var primerNumero = Number(prompt('1 numero'))
+var segundoNumero = Number(prompt('2 numero'))
+
+if (primerNumero < segundoNumero){
+    
+}
